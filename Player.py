fbbbb592952b6864bf268eac959d7aae41959fcc
sdgenna <65458122+sdgenna@users.noreply.github.com>
2020-05-16 11:47:03 -0600

    Update Player.py
    1.1  Init and action define
    1.2 Memory define (basic)

--- Player.py
+++ Player.py
@@ -1,9 +1,6 @@
 import random
 ACTIONS = ['load', 'shoot', 'laser', 'block', 'laserblock']
-
-
 DEBUG = 'True'
-
 
 class Player:
     def __init__(self, name):
@@ -14,11 +11,34 @@ class Player:
     def act(self, player_action_id):
         player_action = ACTIONS[player_action_id]
         if DEBUG:
-            print(self.name + " has " + player_action)
+            print(self.name + " has launched : " + player_action)
         return player_action
 
     def random_act(self):
         player_action = random.choice(ACTIONS)
         if DEBUG:
-            print(self.name + " has " + player_action)
+            print(self.name + " has launched :  " + player_action)
         return player_action
+
+    def read(self,opponent_action):
+        # Convert string into integer
+        switcher  = {
+            "load": 0,
+            "shoot": 1,
+            "laser": 2,
+            "block": 3,
+            "laserblock": 4,
+        }
+
+        return switcher.get(opponent_action,"Action Not Understood")
+
+    def memorize_opponent_action(self, opponent_action_id):
+        self.opponent_memory.append(opponent_action_id)
+        if DEBUG:
+            print(self.name+" has remembered the opponent action : " + ACTIONS[opponent_action_id])
+
+    def memorize_player_action(self, my_action_id):
+        self.player_memory.append(my_action_id)
+        if DEBUG:
+            print(self.name+ " has remembered its own action : " + ACTIONS[my_action_id])
+
