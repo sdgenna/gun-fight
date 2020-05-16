9ca6516a273ada078cb334859da08ac7f805732d
sdgenna <65458122+sdgenna@users.noreply.github.com>
2020-05-16 13:07:01 -0600

    run - added memory of the player in the game

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import Player
 import Tristan
 import Sylvain
 from datetime import datetime
+import time
 import logging
 from sys import stdout
 
@@ -18,14 +19,28 @@ def writeLog(message, logObject):
 def runOneRound(Player1, Player2):
     Player1Ammo = 0
     Player2Ammo = 0
+
     for turnNumber in range(NUM_TURNS_PER_ROUND):
         writeLog("Turn number {0}".format(turnNumber + 1), logger)
+
+
         Player1Action = Player1.act()
         Player2Action = Player2.act()
+
+        NAME = [Player1, Player2]
+        Player1Action_ID = NAME[0].read(Player1Action)
+        Player2Action_ID = NAME[1].read(Player2Action)
+        NAME[0].memorize_own_action(Player1Action_ID)
+        NAME[0].memorize_opponent_action(Player2Action_ID)
+        NAME[1].memorize_own_action(Player2Action_ID)
+        NAME[1].memorize_opponent_action(Player1Action_ID)
+
+
         if Player1Action == Player.ACTIONS[-1]:
             Player1Ammo += 1
         if Player2Action == Player.ACTIONS[-1]:
             Player2Ammo += 1
+
         if Player1Action == Player.ACTIONS[0]:
             if Player1Ammo <= 0:
                 writeLog("{0} loses".format(Player1.name), logger)
@@ -35,6 +50,7 @@ def runOneRound(Player1, Player2):
                 if Player2Action != Player.ACTIONS[2] or Player2Action != Player.ACTIONS[0] or Player2Action != Player.ACTIONS[1]:
                     writeLog("{0} loses".format(Player2.name), logger)
                     break
+
         elif Player1Action == Player.ACTIONS[1]:
             if Player1Ammo <= 1:
                 writeLog("{0} loses".format(Player1.name), logger)
