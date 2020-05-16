e980f8972e30c8ec928bfa3f0b426dceba1fd0ec
sdgenna <65458122+sdgenna@users.noreply.github.com>
2020-05-16 11:23:35 -0600

    Update Player.py
    1.1 - Function init and action define

--- Player.py
+++ Player.py
@@ -1,11 +1,24 @@
 import random
+ACTIONS = ['load', 'shoot', 'laser', 'block', 'laserblock']
 
-ACTIONS = ['shoot', 'laser', 'block', 'laserblock', 'load']
+
+DEBUG = 'True'
 
 
 class Player:
     def __init__(self, name):
         self.name = name
+        self.player_memory = []
+        self.opponent_memory = []
 
-    def act(self):
-        return random.choice(ACTIONS)
+    def act(self, player_action_id):
+        player_action = ACTIONS[player_action_id]
+        if DEBUG:
+            print(self.name + " has " + player_action)
+        return player_action
+
+    def random_act(self):
+        player_action = random.choice(ACTIONS)
+        if DEBUG:
+            print(self.name + " has " + player_action)
+        return player_action
