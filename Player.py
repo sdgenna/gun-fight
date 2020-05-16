c9d786e85e7d221708962d353760d5af8e5ad865
sdgenna <65458122+sdgenna@users.noreply.github.com>
2020-05-16 13:09:56 -0600

    Player - Added some DEBUG message in the function themselves

--- Player.py
+++ Player.py
@@ -1,6 +1,11 @@
+# 1. 1 Player  Name Added
+# 1. 2 Player Memory Added
+# 1. 3 Player Internal Rule will be added (In Progress)
+
+
 import random
-ACTIONS = ['load', 'shoot', 'laser', 'block', 'laserblock']
-DEBUG = 'True'
+ACTIONS = ['shoot', 'laser', 'block', 'laserblock', 'load']
+DEBUG = 'False'
 
 class Player:
     def __init__(self, name):
@@ -8,13 +13,13 @@ class Player:
         self.player_memory = []
         self.opponent_memory = []
 
-    def act(self, player_action_id):
+    def deterministic_act(self, player_action_id):
         player_action = ACTIONS[player_action_id]
         if DEBUG:
             print(self.name + " has launched : " + player_action)
         return player_action
 
-    def random_act(self):
+    def act(self):
         player_action = random.choice(ACTIONS)
         if DEBUG:
             print(self.name + " has launched :  " + player_action)
@@ -23,11 +28,11 @@ class Player:
     def read(self,opponent_action):
         # Convert string into integer
         switcher  = {
-            "load": 0,
-            "shoot": 1,
-            "laser": 2,
-            "block": 3,
-            "laserblock": 4,
+            "shoot": 0,
+            "laser": 1,
+            "block": 2,
+            "laserblock": 3,
+            "load": 4,
         }
 
         return switcher.get(opponent_action,"Action Not Understood")
@@ -36,10 +41,15 @@ class Player:
         self.opponent_memory.append(opponent_action_id)
         if DEBUG:
             print(self.name+" has remembered the opponent action : " + ACTIONS[opponent_action_id])
+            print(self.name + " remembered " + str(self.opponent_memory) + " as the opponent actions")
 
-    def memorize_player_action(self, my_action_id):
+
+    def memorize_own_action(self, my_action_id):
         self.player_memory.append(my_action_id)
         if DEBUG:
             print(self.name+ " has remembered its own action : " + ACTIONS[my_action_id])
+            print(self.name + " remembered " + str(self.player_memory) + " as its own actions")
+
+
 
 
